620b0635d90de8a7a7e832523ce7cd177d984cf4
Caesar Wong <caesar.wongw@gmail.com>
2023-09-07 23:40:52 -0700

    reset to local settings

--- src/app.py
+++ src/app.py
@@ -2,10 +2,8 @@ import json
 from flask import Flask, request, jsonify
 from flasgger import Swagger
 # blueprint imports
-# from resources import resources_bp
-from . import resources
-from . import data_retrieval
-# from data_retrieval import childcare_retrieval_bp
+from resources import resources_bp
+from data_retrieval import childcare_retrieval_bp
 
 app = Flask(__name__)
 swagger = Swagger(app)
